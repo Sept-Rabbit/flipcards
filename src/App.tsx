e3cdfba0d2dc8a3352902412b78d72001c207ab5
Yen-Chen Li <foxitai@TW-Tino-Li.local>
2022-05-22 15:04:26 +0800

    change image routes

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,46 @@ function App() {
   const [cards, setCards] = useState<Card[]>([]);
 
   const imageCards: Card[] = [
-    { source: "./src/images/cubs.jpg", alt: "cubs" },
-    { source: "./src/images/puppy.jpg", alt: "puppy" },
-    { source: "./src/images/kitten.jpg", alt: "kitten" },
-    { source: "./src/images/jenna.jpg", alt: "jenna" },
-    { source: "./src/images/keira.jpg", alt: "keira" },
-    { source: "./src/images/yellow.jpg", alt: "yellow" },
-    { source: "./src/images/sean.jpg", alt: "sean" },
-    { source: "./src/images/albee.jpg", alt: "albee" },
+    {
+      source:
+        "https://drive.google.com/file/d/1-hDwUp1v8dPmv5Grt4umorL1R2881ADb/view?usp=sharing",
+      alt: "cub",
+    },
+    {
+      source:
+        "https://drive.google.com/file/d/1UKeSlgPN2bG2WvMRAYFRm5fR7ECqOuCJ/view?usp=sharing",
+      alt: "puppy",
+    },
+    {
+      source:
+        "https://drive.google.com/file/d/1XnHpsA0Wf2wwNGj0w90L1ZXXCuQoPlPO/view?usp=sharing",
+      alt: "kitten",
+    },
+    {
+      source:
+        "https://drive.google.com/file/d/1WmD3iXrsyRr92xop5Q7tb9d_vPKZQG0f/view?usp=sharing",
+      alt: "jenna",
+    },
+    {
+      source:
+        "https://drive.google.com/file/d/1iLIJm8nzElEJOLCjANfUTOU-5F3ZaAT5/view?usp=sharing",
+      alt: "keira",
+    },
+    {
+      source:
+        "https://drive.google.com/file/d/1MK9LHUSertYh6S92FdT5YXqHePy5C4A9/view?usp=sharing",
+      alt: "yellow",
+    },
+    {
+      source:
+        "https://drive.google.com/file/d/1Vt7BFVIocf4J2zm_aa1OmNpr1RlEWFdW/view?usp=sharing",
+      alt: "sean",
+    },
+    {
+      source:
+        "https://drive.google.com/file/d/10yX9NLpyqe5apS1r4Q8w_YY4vFFTFZT6/view?usp=sharing",
+      alt: "albee",
+    },
   ];
 
   const shuffleCards = () => {
